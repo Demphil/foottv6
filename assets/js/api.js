f347fdf93f7bcf69b46bed08c8c0e2a83d9d6155
Demphil <92610592+Demphil@users.noreply.github.com>
2025-04-29 17:02:29 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,13 +1,14 @@
+// api.js
 
 const API_URL = 'https://api-football-v1.p.rapidapi.com/v3/fixtures';
-const API_KEY = '3677c62bbcmshe54df743c38f9f5p13b6b9jsn4e20f3d1255';
+const API_KEY = '3677c62bbcmshe54df743c38f9f5p13b6b9jsn4e20f3d12556'; // ← غيّر هذا السطر بمفتاحك الحقيقي
 const API_HOST = 'api-football-v1.p.rapidapi.com';
 
-const leagueIds = [2, 39, 140, 135, 61, 78, 307, 200]; // الدوريات
+const leagueIds = [2, 39, 140, 135, 61, 78, 307, 200]; // الدوريات المطلوبة
 const season = 2023;
 const timezone = 'Africa/Casablanca';
 
-export const fetchAllLeaguesMatches = async () => {
+export const fetchMatches = async () => {
     try {
         const requests = leagueIds.map(id =>
             fetch(`${API_URL}?league=${id}&season=${season}&timezone=${timezone}`, {
