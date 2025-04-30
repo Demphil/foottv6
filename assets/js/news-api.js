2b8ec38cc7a50de177e1d09caeed0f2d44ba9fbb
Demphil <92610592+Demphil@users.noreply.github.com>
2025-04-30 23:42:16 +0100

    Update news-api.js

--- assets/js/news-api.js
+++ assets/js/news-api.js
@@ -2,7 +2,7 @@
 
 const apikey = '320e688cfb9682d071750f4212f83753';
 const category = 'general'; // يمكنك تغيير الفئة حسب الحاجة
-const url = `https://gnews.io/api/v4/top-headlines?sports=general&apikey=320e688cfb9682d071750f4212f83753`;
+const url = `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=us&max=10&apikey=${apikey}`;;
 
 async function fetchNews() {
   try {
