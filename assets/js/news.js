6dc609f4c44b3d31340b1449f52cbafbd842c5d0
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-01 19:48:39 +0100

    Update news.js

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,5 +1,28 @@
 //=================== الإعدادات ===================
-const apiKey       = '320e688cfb9682d071750f4212f83753';
+const apiKey   ='c3043545e8b02be6502326236791500f';
+category = 'general';
+url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apikey;
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    articles = data.articles;
+
+    for (i = 0; i < articles.length; i++) {
+      // articles[i].title
+      console.log("Title: " + articles[i]['title']);
+      // articles[i].description
+      console.log("Description: " + articles[i]['description']);
+      // You can replace {property} below with any of the article properties returned by the API.
+      // articles[i].{property}
+      // console.log(articles[i]['{property}']);
+
+      // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
+      break;
+    }
+  });;
 const baseUrl      = 'https://gnews.io/api/v4';
 const language     = 'ar';
 const country      = 'eg';
