dfc96a6ba1e246a62315f48e2167727061a84fc4
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-07 04:05:53 +0100

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -292,10 +292,10 @@ document.addEventListener('DOMContentLoaded', async () => {
             'bein-sports-hd6': 'bein SPORTS HD6',
             'ad-sports-premium1': 'AD SPORTS PREMIUM1',
             'Arryadia-SD/HD': 'ARRYADIA-SD/HD',
-                 'SSC 1 HD': 'ssc1-hd',
-    'SSC EXTRA 2': 'ssc-extra2',
-    'SSC EXTRA 1': 'ssc-extra1',
-    'SSC EXTRA 3': 'ssc-extra3',
+                 'SSC 1HD': 'ssc1-hd',
+    'SSC EXTRA2': 'ssc-extra2',
+    'SSC EXTRA1': 'ssc-extra1',
+    'SSC EXTRA3': 'ssc-extra3',
         };
 
         return broadcastData
@@ -317,10 +317,10 @@ document.addEventListener('DOMContentLoaded', async () => {
             'bein SPORTS HD2': 'bein-sports-hd2',
             'bein SPORTS HD3': 'bein-sports-hd3',
             'AD SPORTS PREMIUM1': 'ad-sports-premium1'
-                 'SSC 1 HD': 'ssc1-hd',
-    'SSC EXTRA 2': 'ssc-extra2',
-    'SSC EXTRA 1': 'ssc-extra1',
-    'SSC EXTRA 3': 'ssc-extra3',
+                 'SSC 1HD': 'ssc1-hd',
+    'SSC EXTRA2': 'ssc-extra2',
+    'SSC EXTRA1': 'ssc-extra1',
+    'SSC EXTRA3': 'ssc-extra3',
         };
         return ARABIC_CHANNELS[channelName] || '';
     }
