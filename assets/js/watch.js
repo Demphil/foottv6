a8c6e2c27190f9e9b741da92b0f0d8053c661b52
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-08 03:11:14 +0100

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -214,7 +214,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             'ssc-extra2': 'https://watch.3rbcafee.com/2024/10/sscnew-prem.html?id=SSC_EXTRA2',
             'ssc-extra3': 'https://watch.3rbcafee.com/2024/10/sscnew-prem.html?id=SSC_EXTRA3',
             'Arryadia-SD/HD': 'https://snrt.player.easybroadcast.io/events/73_arryadia_k2tgcj0',
-             'almaghribia':  'https://snrt.player.easybroadcast.io/events/73_almaghribia_83tz85q',
+             'al-maghribia':  'https://snrt.player.easybroadcast.io/events/73_almaghribia_83tz85q',
             
         };
 
@@ -291,12 +291,12 @@ document.addEventListener('DOMContentLoaded', async () => {
             'bein-sports-hd5': 'bein SPORTS HD5',
             'bein-sports-hd6': 'bein SPORTS HD6',
             'ad-sports-premium1': 'AD SPORTS PREMIUM1',
-            'Arryadia-SD/HD': 'ARRYADIA-SD/HD',
+            'Arryadia-SD/HD': 'arryadia sd/hd',
             'SSC-HD1': 'ssc hd1',
             'SSC-EXTRA2': 'ssc extra2',
             'SSC-EXTRA1': 'ssc extra1',
             'SSC-EXTRA3': 'ssc extra3',
-            'AL-MAGHRIBIA' : 'al maghribia',
+            'al-maghribia': 'AL MAGHRIBIA',
         };
 
         return broadcastData
@@ -322,8 +322,8 @@ document.addEventListener('DOMContentLoaded', async () => {
             'SSC EXTRA2': 'ssc-extra2',
             'SSC EXTRA1': 'ssc-extra1',
             'SSC EXTRA3': 'ssc-extra3',
-            'arryadia sd/hd': 'Arryadia SD/HD',
-            'AL-MAGHRIBIA' : 'al maghribia',
+            'arryadia sd/hd': 'Arryadia-SD/HD',
+            'AL MAGHRIBIA' : 'al-maghribia',
 
         };
         return ARABIC_CHANNELS[channelName] || '';
