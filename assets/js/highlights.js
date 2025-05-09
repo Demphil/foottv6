8ab6134ab6cb0057520cacb775f5ef077b7ec397
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-10 00:05:28 +0100

    Update highlights.js

--- assets/js/highlights.js
+++ assets/js/highlights.js
@@ -10,7 +10,7 @@ const LEAGUES = [
     'Serie A',
     'Ligue 1',
     'Champions League',
-    'UEFA Europa Conference League'
+    'UEFA Europa Conference League',
 ];
 
 const getCachedData = () => {
