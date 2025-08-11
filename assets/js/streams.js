e6e7acc17b527ece5e57507afd449283cd11631a
Demphil <92610592+Demphil@users.noreply.github.com>
2025-08-11 21:16:59 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -10,12 +10,12 @@ export const streamLinks = {
   'beIN SPORTS HD 1': 'https://foot966.blogspot.com/p/bein1.html',
   'beIN SPORTS HD 2': 'https://foot966.blogspot.com/p/bein2.html',
   'beIN SPORTS HD 3': 'https://foot966.blogspot.com/p/bein3_10.html',
-  'beIN SPORTS HD 4': 'https://foot966.blogspot.com/p/bein3_10.html',
-  'beIN SPORTS HD 5': 'https://foot966.blogspot.com/p/bein3_10.html',
-  'beIN SPORTS HD 6': 'https://foot966.blogspot.com/p/bein3_10.html',
-  'SSC 1 HD': 'https://your-blog-name.blogspot.com/p/ssc-1.html',
-  'SSC 2 HD': 'https://your-blog-name.blogspot.com/p/ssc-1.html',
-  'SSC 3 HD': 'https://your-blog-name.blogspot.com/p/ssc-1.html',
+  'beIN SPORTS HD 4': 'https://foot966.blogspot.com/p/bein4.html',
+  'beIN SPORTS HD 5': 'https://foot966.blogspot.com/p/bein5.html',
+  'beIN SPORTS HD 6': 'https://foot966.blogspot.com/p/bein6.html',
+  'SSC 1 HD': 'https://foot966.blogspot.com/p/ssc1.html',
+  'SSC 2 HD': 'https://foot966.blogspot.com/p/ssc2.html',
+  'SSC 3 HD': 'https://foot966.blogspot.com/p/ssc3.html',
 
   // =================================================================
   // === الخطة البديلة: الربط اليدوي عبر أسماء الفرق (في حال عدم توفر اسم القناة) ===
@@ -25,6 +25,7 @@ export const streamLinks = {
   
   'ريال مدريد-برشلونة': 'https://your-blog-name.blogspot.com/p/el-clasico.html',
   'أورلاندو سيتي-انتر ميامي': 'https://foot966.blogspot.com/p/bein3_10.html',
+
 
   // أضف المزيد من القنوات والمباريات المخصصة هنا
 };
