6768ab2ed51e2aa41e8e36053b311d61c5f007df
Demphil <92610592+Demphil@users.noreply.github.com>
2025-08-15 04:10:52 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -10,7 +10,7 @@ export const streamLinks = {
   'beIN SPORTS HD 1': 'https://foot966.blogspot.com/p/bein1.html',
   'beIN SPORTS HD 2': 'https://foot966.blogspot.com/p/bein2.html',
   'beIN SPORTS HD 3': 'https://foot966.blogspot.com/p/bein3_10.html',
-  'beIN SPORTS 3 HD': 'https://foot966.blogspot.com/p/bein3_10.html',
+  'beIN Sports 3 HD': 'https://foot966.blogspot.com/p/bein3_10.html',
   'beIN SPORTS HD 4': 'https://foot966.blogspot.com/p/bein4.html',
   'beIN SPORTS HD 5': 'https://foot966.blogspot.com/p/bein5.html',
   'beIN SPORTS HD 6': 'https://foot966.blogspot.com/p/bein6.html',
