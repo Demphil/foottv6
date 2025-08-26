bc54a045de2c57dfbff87f14172561bb5b96c27f
Demphil <92610592+Demphil@users.noreply.github.com>
2025-08-26 13:30:18 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -38,9 +38,9 @@ export const streamLinks = {
   // القيمة: هو رابط صفحة Blogger التي أنشأتها خصيصًا لهذه المباراة
   
   'ليفانتي-برشلونة': 'https://foot966.blogspot.com/p/bein2.html',
-  'الجزائر-السودان': 'https://foot966.blogspot.com/p/bein6.html',
+  'مدغشقر-السودان': 'https://foot966.blogspot.com/p/bein6.html',
   'ريال بيتيس-ألافيس': 'https://foot966.blogspot.com/p/bein3_15.html',
-  'تنزانيا-المغرب': 'https://foot966.blogspot.com/p/bein6.html',
+  'السنغال-المغرب': 'https://foot966.blogspot.com/p/bein6.html',
   'وست هام يونايتد-تشيلسي': 'https://foot966.blogspot.com/p/bein1.html',
   'ميلان-كريمونيسي': 'https://foot966.blogspot.com/p/ad-sports-premium-1.html',
   'روما-بولونيا': 'https://foot966.blogspot.com/p/ad-sports-premium-2.html',
