3b3729bc876889f245acd8dd7d150b5bd950645f
Demphil <92610592+Demphil@users.noreply.github.com>
2025-10-03 18:15:18 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -43,7 +43,7 @@ export const streamLinks = {
   'Arryadia S/D': 'https://demphil.github.io/aryadialive/',
   'الرياضية المغربية': 'https://demphil.github.io/aryadialive/',
   'الرياضية المغربية 1': 'https://demphil.github.io/aryadialive/',
-  'الرياضية المغربية 3': 'https://demphil.github.io/aryadialive/',
+  'الرياضية المغربية (3)': 'https://demphil.github.io/aryadialive/',
   'الرياضية المغربية 2': 'https://demphil.github.io/aryadialive/',
   'ON TIME SPORTS 1': 'https://demphil.github.io/live-stream/',
   'أون سبورت 1': 'https://demphil.github.io/live-stream/',
