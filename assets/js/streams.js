ac0cef56e8e9f44f703f1ae978644d5d852cec24
Demphil <92610592+Demphil@users.noreply.github.com>
2025-11-03 16:31:29 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -75,7 +75,7 @@ export const streamLinks = {
   'روما-فيكتوريا بلزن': 'https://demphil.github.io/foottv-bein3//',
   'بيراميدز-نهظة بركان': 'https://demphil.github.io/football-live/',
   'أتلتيكو مدريد-ديبورتيفو ألافيس': 'https://foot966.blogspot.com/p/bein4.html',
-  'اليابان تحت 17-المغرب تحت 17': 'https://demphil.github.io/aryadialive/',
+  'تونس تحت 17-فيجي تحت 17': 'https://demphil.github.io/aryadialive/',
   'ليل-أنجيه': 'https://demphil.github.io/bein4/',
   'أشانتي كوتوكو-الوداد الرياضي': 'https://demphil.github.io/algery/',
   'المغرب-الرجاء البيضاوي': 'https://demphil.github.io/aryadialive/',
