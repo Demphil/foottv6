2726b68fa365df4e844252b8e4a2f1cc42fb2263
Demphil <92610592+Demphil@users.noreply.github.com>
2025-11-17 17:55:04 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -60,7 +60,7 @@ export const streamLinks = {
   'MBC Action': 'https://demphil.github.io/mbc-action/',
   'beIN Sports HD': 'https://demphil.github.io/beinfree/',
   'beIN SPORTS HD': 'https://demphil.github.io/beinfree/',
-  // 'Starzplay': 'https://demphil.github.io/starzplay/',//
+  'الكأس HD1': 'https://dns2.getmoney.my/albaplayer/b2/?serv=0',
    'الجزائرية السادسة': 'https://demphil.github.io/algy6/',
   'beIN Sports /Xtra 1': 'https://demphil.github.io/BEINEXTRA1/',
   
