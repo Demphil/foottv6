fa2ef4dc9690656a00ff7dba95018dbeb4c4c115
Demphil <92610592+Demphil@users.noreply.github.com>
2025-11-17 17:52:19 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -78,7 +78,7 @@ export const streamLinks = {
   'الولايات المتحدة تحت 17-المغرب تحت 17': 'https://demphil.github.io/beinfree/',
   'ليل-أنجيه': 'https://demphil.github.io/bein4/',
   'أشانتي كوتوكو-الوداد الرياضي': 'https://demphil.github.io/algery/',
-  'البرازيل-السنغال': 'https://demphil.github.io/abu-dhabi-sport12/',
+  'مصر-الجزائر': 'https://demphil.github.io/onesports2/',
 
   // أضف المزيد من القنوات والمباريات المخصصة هنا
 };
