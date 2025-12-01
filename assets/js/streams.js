55d39a99396555113397296383ef0bd57ddd4956
Demphil <92610592+Demphil@users.noreply.github.com>
2025-12-01 03:39:07 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -79,8 +79,8 @@ export const streamLinks = {
   'أتلتيكو مدريد-ديبورتيفو ألافيس': 'https://foot966.blogspot.com/p/bein4.html',
   'المغرب تحت 17-البرازيل تحت 17': 'https://demphil.github.io/beinfree/',
   'الترجي-ستاد مالي': 'https://demphil.github.io/bein7/',
-  'الأهلي-شبيبة القبائل ': 'https://demphil.github.io/bein6/',
-  'الرأس الأخضر-مصر': 'https://demphil.github.io/abu-dhabi-sport12/',
+  'تونس-سوريا': 'https://demphil.github.io/beinfree/',
+  'قطر-فلسطين': 'https://demphil.github.io/beinfree/',
 
   // أضف المزيد من القنوات والمباريات المخصصة هنا
 };
