0d06044c93f5fa065bf035ca741e273c6bf84335
Demphil <92610592+Demphil@users.noreply.github.com>
2025-12-11 01:17:13 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -75,7 +75,7 @@ export async function getTodayMatches() {
     return cachedMatches;
   }
   console.log("🌐 Fetching today's matches from network.");
-  const targetUrl = 'https://kooora1live.com/';
+  const targetUrl = 'https://m.yallasport.onl/';
   const newMatches = await fetchMatches(targetUrl);
   if (newMatches.length > 0) setCache(CACHE_KEY_TODAY, newMatches);
   return newMatches;
@@ -88,7 +88,7 @@ export async function getTomorrowMatches() {
     return cachedMatches;
   }
   console.log("🌐 Fetching tomorrow's matches from network.");
-  const targetUrl = 'https://kooora1live.com/tomorrow-matches/';
+  const targetUrl = 'https://m.yallasport.onl/matches-tomorrow/';
   const newMatches = await fetchMatches(targetUrl);
   if (newMatches.length > 0) setCache(CACHE_KEY_TOMORROW, newMatches);
   return newMatches;
@@ -169,5 +169,5 @@ function extractImageUrl(imgElement) {
   if (!imgElement) return '';
   const src = imgElement.dataset.src || imgElement.getAttribute('src') || '';
   if (src.startsWith('http') || src.startsWith('//')) return src;
-  return `https://kooora1live.com/${src.startsWith('/') ? '' : '/'}${src}`;
+  return `https://m.yallasport.onl/${src.startsWith('/') ? '' : '/'}${src}`;
 }
