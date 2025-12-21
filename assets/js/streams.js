13d4f7bb6de43675229a5595da75bf4b17306bc6
Demphil <92610592+Demphil@users.noreply.github.com>
2025-12-21 19:07:56 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -84,7 +84,7 @@ export const streamLinks = {
   'جورجيا-إسبانيا': 'https://demphil.github.io/beinsports1/',
   'تركيا-بلغاريا': 'https://demphil.github.io/football-live/',
   'أتلتيكو مدريد-ديبورتيفو ألافيس': 'https://foot966.blogspot.com/p/bein4.html',
-  'المغرب-جزر القمر': 'https://demphil.github.io/aryadialive/',
+  //'المغرب-جزر القمر': 'https://demphil.github.io/aryadialive/',
   'الترجي-ستاد مالي': 'https://demphil.github.io/bein7/',
   'تونس-سوريا': 'https://demphil.github.io/BEINEXTRA1/',
   'قطر-تونس': 'https://demphil.github.io/abu-dhabi-sport12/',
