b93e6e53a5cbf4ba3185fbc839c261f071ee3071
Demphil <92610592+Demphil@users.noreply.github.com>
2025-12-31 15:55:04 +0100

    Update match link from Algeria to Equatorial Guinea

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -89,7 +89,7 @@ export const streamLinks = {
  'المغرب-مالي': 'https://demphil.github.io/BEINmax1/',
   'الترجي-ستاد مالي': 'https://demphil.github.io/bein7/',
   'مصر-جنوب أفريقيا': 'https://demphil.github.io/BEINmax1/',
-  'الجزائر-بوركينا فاسو': 'https://demphil.github.io/BEINmax1/',
+  'غينيا الاستوائية-الجزائر': 'https://demphil.github.io/BEINmax1/',
 
   // أضف المزيد من القنوات والمباريات المخصصة هنا
 };
