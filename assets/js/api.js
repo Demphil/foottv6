a0b1f68e96a7df9b72c0bf2427ee1c8ecf8860c6
Demphil <92610592+Demphil@users.noreply.github.com>
2026-03-31 21:08:12 +0100

    Update target URLs for fetching matches

--- assets/js/api.js
+++ assets/js/api.js
@@ -75,7 +75,7 @@ export async function getTodayMatches() {
     return cachedMatches;
   }
   console.log("🌐 Fetching today's matches from network.");
-  const targetUrl = 'https://www.kooralive24.site/';
+  const targetUrl = 'https://www.kooorasport.live/';
   const newMatches = await fetchMatches(targetUrl);
   if (newMatches.length > 0) setCache(CACHE_KEY_TODAY, newMatches);
   return newMatches;
@@ -88,7 +88,7 @@ export async function getTomorrowMatches() {
     return cachedMatches;
   }
   console.log("🌐 Fetching tomorrow's matches from network.");
-  const targetUrl = 'https://www.kooralive24.site/matches-tomorrow/';
+  const targetUrl = 'https://www.kooorasport.live/matches-tomorrow/';
   const newMatches = await fetchMatches(targetUrl);
   if (newMatches.length > 0) setCache(CACHE_KEY_TOMORROW, newMatches);
   return newMatches;
@@ -169,5 +169,5 @@ function extractImageUrl(imgElement) {
   if (!imgElement) return '';
   const src = imgElement.dataset.src || imgElement.getAttribute('src') || '';
   if (src.startsWith('http') || src.startsWith('//')) return src;
-  return `https://www.kooralive24.site/${src.startsWith('/') ? '' : '/'}${src}`;
+  return `https://www.kooorasport.live/${src.startsWith('/') ? '' : '/'}${src}`;
 }
