7626b8ff4b20f0f47bef8b25445aeb21c67049dd
Demphil <92610592+Demphil@users.noreply.github.com>
2026-06-25 01:16:22 +0100

    Fix hour adjustment for UTC+3 to UTC+1
    Adjust hours calculation for time zone difference.

--- assets/js/api.js
+++ assets/js/api.js
@@ -50,7 +50,7 @@ function convertSourceToMoroccoTime(timeString) {
     }
 
     // طرح ساعتين (توقيت مكة UTC+3 مقابل توقيت المغرب الحالي UTC+1)
-    hours -= 0; 
+    hours -= 2; 
 
     if (hours < 0) {
       hours += 24;
