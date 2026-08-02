acf0a13866b9a3f011c14e8dc371b9812c6bd87f
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-02 20:02:51 +0100

    Update BASE_SITE_URL to new streaming site

--- assets/js/api.js
+++ assets/js/api.js
@@ -55,7 +55,7 @@ function convertSourceToMoroccoTime(timeString) {
 
 // --- 3. API Functions ---
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
-const BASE_SITE_URL = 'https://yacine-tv.net/';
+const BASE_SITE_URL = 'https://mcity.live/';
 
 export async function getTodayMatches() {
   const cachedMatches = getCache(CACHE_KEY_TODAY);
