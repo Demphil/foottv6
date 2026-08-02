7589482611917929fd1069622fd0c2889e4cb644
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-02 19:48:19 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -55,7 +55,7 @@ function convertSourceToMoroccoTime(timeString) {
 
 // --- 3. API Functions ---
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
-const BASE_SITE_URL = 'https://footnow.info/';
+const BASE_SITE_URL = 'https://yalla-pk.com/';
 
 export async function getTodayMatches() {
   const cachedMatches = getCache(CACHE_KEY_TODAY);
