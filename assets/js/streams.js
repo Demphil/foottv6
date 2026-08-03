0b0f0e222de03c422cd0138052b495e04a632788
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-03 21:02:00 +0100

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -112,7 +112,7 @@ export const streamLinks = {
   
   'ريال مدريد-فيورنتينا': 'https://demphil.github.io/arryadia-s-d/',
   'السنغال - سيدات-المغرب - سيدات': 'https://demphil.github.io/bein6/',
-  'كينيا - سيدات-الجزائر - سيدات': 'https://foot966.blogspot.com/p/bein4.html',
+  'كينيا - سيدات-الجزائر - سيدات': 'https://demphil.github.io/bein5/',
  'المغرب-مالي': 'https://demphil.github.io/BEINmax1/',
   'السنغال-مصر': 'https://demphil.github.io/BEINmax1/',
   'كندا-المغرب': 'https://demphil.github.io/BEINmax1/',
