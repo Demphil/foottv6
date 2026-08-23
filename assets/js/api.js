f372d52f12cd0bed343b168da0d7e6d8f14b7d1c
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-23 22:16:47 +0100

    Update BASE_SITE_URL to new domain

--- assets/js/api.js
+++ assets/js/api.js
@@ -54,7 +54,7 @@ function convertSourceToMoroccoTime(timeString) {
 
 // --- 3. API Functions ---
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
-const BASE_SITE_URL = 'https://koorashoo.com/';
+const BASE_SITE_URL = 'https://koora-live-max.com/';
 
 export async function getTodayMatches() {
   const cachedMatches = getCache(CACHE_KEY_TODAY);
