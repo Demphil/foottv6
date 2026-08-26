6742d8f220349ffb1e0a670a33bdc670d49e6c0b
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-26 21:16:04 +0100

    Update BASE_SITE_URL to new sports site

--- assets/js/api.js
+++ assets/js/api.js
@@ -54,7 +54,7 @@ function convertSourceToMoroccoTime(timeString) {
 
 // --- 3. API Functions ---
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
-const BASE_SITE_URL = 'https://koora-live-plus.one/';
+const BASE_SITE_URL = 'https://sport-live.pl/';
 
 export async function getTodayMatches() {
   const cachedMatches = getCache(CACHE_KEY_TODAY);
