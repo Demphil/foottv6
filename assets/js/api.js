459db46cfe2478a19cf7088a353ca5c1836ed772
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-29 21:09:24 +0100

    Update BASE_SITE_URL for API requests

--- assets/js/api.js
+++ assets/js/api.js
@@ -71,7 +71,7 @@ function convertSourceToMoroccoTime(timeString) {
 // --- 3. API Functions ---
 
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
-const BASE_SITE_URL = 'https://tvkoralive.com';
+const BASE_SITE_URL = 'https://koora-live-pluss.online/';
 
 export async function getTodayMatches() {
 
