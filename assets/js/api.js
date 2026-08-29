a9363cc1e367619ea17efdf17a6c5289394b8c90
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-29 19:59:43 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -54,7 +54,7 @@ function convertSourceToMoroccoTime(timeString) {
 
 // --- 3. API Functions ---
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
-const BASE_SITE_URL = 'https://sport-live.pl/';
+const BASE_SITE_URL = 'https://livehd70.live/';
 
 export async function getTodayMatches() {
   const cachedMatches = getCache(CACHE_KEY_TODAY);
