1e2254b0bac22fbd3c598fa2e9aa51839f818a98
Demphil <92610592+Demphil@users.noreply.github.com>
2026-08-30 16:35:51 +0100

    Update BASE_SITE_URL for API calls

--- assets/js/api.js
+++ assets/js/api.js
@@ -110,7 +110,7 @@ function convertSourceToMoroccoTime(timeString) {
 
 const PROXY_URL = 'https://foottv-proxy-1.koora-live.workers.dev/?url=';
 
-const BASE_SITE_URL = 'https://bein-match.homes/';
+const BASE_SITE_URL = 'https://bein-match.homes/yalla-shoot-tv/';
 
 
 
