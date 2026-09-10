d3340309b0273e9bcddadc06f32b1123ffe80f1b
Demphil <92610592+Demphil@users.noreply.github.com>
2026-09-10 18:31:21 +0100

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -222,7 +222,7 @@ function renderServers(streams, playerContainer, playerLoader, serversContainer)
         topBar.appendChild(noticeMsg);
     }
 
-    loadPlayer(streams[0], playerContainer, playerLoader);
+    loadPlayer(streams[2], playerContainer, playerLoader);
 }
 function loadPlayer(stream, container, loader) {
     container.innerHTML = ''; 
