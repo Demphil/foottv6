965fdaabfa9d9365cfa99423404f8c161ad5dbe0
Demphil <92610592+Demphil@users.noreply.github.com>
2026-09-26 16:19:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11820351
+    "zoneId": 11898534
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
